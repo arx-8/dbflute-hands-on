280275b130dba2d00abb5a7e32939f73176ad07f
swan0 <swans.randomness@gmail.com>
2015-10-15 22:55:31 +0900

    start s8

---
+++ dbflute-hands-on/src/main/java/org/docksidestage/handson/logic/HandsOn08Logic.java
@@ -0,0 +1,25 @@
+package org.docksidestage.handson.logic;
+
+/**
+ * @author swan0
+ *
+ */
+public class HandsOn08Logic {
+
+    // ===================================================================================
+    //                                                                             Execute
+    //                                                                             =======
+    // -----------------------------------------------------
+    //                                    排他制御ありの更新
+    //                                                ------
+    /**
+     * 指定された会員を正式会員に更新する
+     * 排他制御ありで更新する
+     * 引数の値で null は許されない
+     * @param memberId
+     * @param versionNo
+     */
+    public void updateMemberChangedToFormalized(Integer memberId, Long versionNo) {
+
+    }
+}
---
+++ dbflute-hands-on/src/test/java/org/docksidestage/handson/logic/HandsOn08LogicTest.java
@@ -0,0 +1,34 @@
+package org.docksidestage.handson.logic;
+
+/**
+ * @author swan0
+ *
+ */
+public class HandsOn08LogicTest {
+
+    /**
+     * 任意の仮会員の会員IDとバージョンNOを渡して更新すること
+     * 更新処理後、DB上のデータが更新されていることをアサート
+     * @throws Exception
+     */
+    public void test_updateMemberChangedToFormalized_会員が更新されていること() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+
+        // ## Assert ##
+    }
+
+    /**
+     * 何かしらの方法で排他制御例外を発生させてみること
+     * 排他制御例外の内容をログに出力して目視確認すること
+     * @throws Exception
+     */
+    public void test_updateMemberChangedToFormalized_排他制御例外が発生すること() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+
+        // ## Assert ##
+    }
+}
