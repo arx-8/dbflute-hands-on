86a089e7f66ae4c1ff4679425a547fc60b747596
swan0 <swans.randomness@gmail.com>
2015-10-17 01:39:39 +0900

    doing s8

--- dbflute-hands-on/src/main/java/org/docksidestage/handson/logic/HandsOn08Logic.java
+++ dbflute-hands-on/src/main/java/org/docksidestage/handson/logic/HandsOn08Logic.java
@@ -1,10 +1,20 @@
 package org.docksidestage.handson.logic;
 
+import javax.annotation.Resource;
+
+import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
+import org.docksidestage.handson.dbflute.exentity.Member;
+
 /**
  * @author swan0
  *
  */
-public class HandsOn08Logic {
+public class HandsOn08Logic extends BsLogicLogger {
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    @Resource
+    protected MemberBhv memberBhv;
 
     // ===================================================================================
     //                                                                             Execute
@@ -18,8 +28,17 @@ public class HandsOn08Logic {
      * 引数の値で null は許されない
      * @param memberId
      * @param versionNo
+     * @throws IllegalAccessException
      */
-    public void updateMemberChangedToFormalized(Integer memberId, Long versionNo) {
+    public void updateMemberChangedToFormalized(Integer memberId, Long versionNo) throws IllegalAccessException {
+        if (memberId == null || versionNo == null) {
+            throw new IllegalAccessException("引数の値で null は許されない");
+        }
 
+        Member member = new Member();
+        member.setMemberId(memberId);
+        member.setMemberStatusCode_正式会員();
+        member.setVersionNo(versionNo);
+        memberBhv.update(member);
     }
 }
--- dbflute-hands-on/src/test/java/org/docksidestage/handson/logic/HandsOn08LogicTest.java
+++ dbflute-hands-on/src/test/java/org/docksidestage/handson/logic/HandsOn08LogicTest.java
@@ -1,11 +1,42 @@
 package org.docksidestage.handson.logic;
 
+import javax.annotation.Resource;
+
+import org.dbflute.exception.EntityAlreadyUpdatedException;
+import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
+import org.docksidestage.handson.dbflute.exentity.Member;
+import org.docksidestage.handson.unit.UnitContainerTestCase;
+
 /**
  * @author swan0
  *
  */
-public class HandsOn08LogicTest {
+public class HandsOn08LogicTest extends UnitContainerTestCase {
+    // ===================================================================================
+    //                                                                           Attribute
+    //                                                                           =========
+    protected HandsOn08Logic logic;
 
+    @Resource
+    protected MemberBhv memberBhv;
+
+    // ===================================================================================
+    //                                                                          Initialize
+    //                                                                          ==========
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        logic = new HandsOn08Logic();
+        inject(logic);
+        log("Called setUp()");
+    }
+
+    // ===================================================================================
+    //                                                                             Execute
+    //                                                                             =======
+    // -----------------------------------------------------
+    //                                    排他制御ありの更新
+    //                                                ------
     /**
      * 任意の仮会員の会員IDとバージョンNOを渡して更新すること
      * 更新処理後、DB上のデータが更新されていることをアサート
@@ -13,10 +44,23 @@ public class HandsOn08LogicTest {
      */
     public void test_updateMemberChangedToFormalized_会員が更新されていること() throws Exception {
         // ## Arrange ##
+        // 更新する仮会員を適当にselect
+        Member memberBeforeUpdate = memberBhv.selectEntityWithDeletedCheck(cb -> {
+            cb.query().setMemberStatusCode_Equal_仮会員();
+            cb.fetchFirst(1);
+        });
 
         // ## Act ##
+        logic.updateMemberChangedToFormalized(memberBeforeUpdate.getMemberId(), memberBeforeUpdate.getVersionNo());
 
         // ## Assert ##
+        Member memberAfterUpdate = memberBhv.selectEntityWithDeletedCheck(cb -> {
+            cb.query().setMemberId_Equal(memberBeforeUpdate.getMemberId());
+        });
+        log(memberBeforeUpdate);
+        log(memberAfterUpdate);
+        assertFalse(memberAfterUpdate.getVersionNo().equals(memberBeforeUpdate.getVersionNo()));
+        assertTrue(memberAfterUpdate.isMemberStatusCode正式会員());
     }
 
     /**
@@ -26,9 +70,22 @@ public class HandsOn08LogicTest {
      */
     public void test_updateMemberChangedToFormalized_排他制御例外が発生すること() throws Exception {
         // ## Arrange ##
+        String assertMarker = "passed catch";
+
+        // 更新する仮会員を適当にselect
+        Member memberBeforeUpdate = memberBhv.selectEntityWithDeletedCheck(cb -> {
+            cb.query().setMemberStatusCode_Equal_仮会員();
+            cb.fetchFirst(1);
+        });
 
         // ## Act ##
-
         // ## Assert ##
+        try {
+            logic.updateMemberChangedToFormalized(memberBeforeUpdate.getMemberId(), memberBeforeUpdate.getVersionNo() + 1L);
+        } catch (Exception e) {
+            markHere(assertMarker);
+            assertEquals(EntityAlreadyUpdatedException.class, e.getClass());
+        }
+        assertMarked(assertMarker);
     }
 }
